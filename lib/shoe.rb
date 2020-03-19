42e19862c61430425b06c52e0b55e467b01c91d6
Rachel Padworski <rachelpadworski@gmail.com>
2020-03-19 04:13:04 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -23,5 +23,21 @@ class Shoe
   def size
     @size
   end
+  
+  def material=(material)
+    @material = material
+  end
+  
+  def material
+    @material
+  end
+  
+  def condition=(condition)
+    @condition = condition
+  end
+  
+  def condition
+    @condition
+  end
     
 end
